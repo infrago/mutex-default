d46b6dcf4a646259666d5f1792e87cf59e1385d2
yatniel <yatniel@qq.com>
2023-03-22 00:15:36 +0800

    驱动实例化

--- mutex.go
+++ mutex.go
@@ -13,10 +13,9 @@ import (
 type (
 	defaultDriver  struct{}
 	defaultConnect struct {
-		name    string
-		config  mutex.Config
-		setting defaultSetting
-		locks   sync.Map
+		instance *mutex.Instance
+		setting  defaultSetting
+		locks    sync.Map
 	}
 	defaultSetting struct {
 	}
@@ -25,29 +24,29 @@ type (
 	}
 )
 
-func (driver *defaultDriver) Connect(name string, config mutex.Config) (mutex.Connect, error) {
+func (driver *defaultDriver) Connect(inst *mutex.Instance) (mutex.Connect, error) {
 	setting := defaultSetting{}
 	return &defaultConnect{
-		name: name, config: config, setting: setting,
+		instance: inst, setting: setting,
 	}, nil
 }
 
 // 打开连接
 // 待处理，需要一个定时器，定期清理过期的数据
-func (connect *defaultConnect) Open() error {
+func (this *defaultConnect) Open() error {
 	return nil
 }
 
 // 关闭连接
-func (connect *defaultConnect) Close() error {
+func (this *defaultConnect) Close() error {
 	return nil
 }
 
 // 待优化，加上超时设置
-func (connect *defaultConnect) Lock(key string, expiry time.Duration) error {
+func (this *defaultConnect) Lock(key string, expiry time.Duration) error {
 	now := time.Now()
 
-	if vv, ok := connect.locks.Load(key); ok {
+	if vv, ok := this.locks.Load(key); ok {
 		if tm, ok := vv.(defaultValue); ok {
 			if tm.Expiry.UnixNano() > now.UnixNano() {
 				return errors.New("existed")
@@ -56,18 +55,18 @@ func (connect *defaultConnect) Lock(key string, expiry time.Duration) error {
 	}
 
 	if expiry <= 0 {
-		expiry = connect.config.Expiry
+		expiry = this.instance.Config.Expiry
 	}
 
 	value := defaultValue{
-		Expiry: now.Add(connect.config.Expiry),
+		Expiry: now.Add(this.instance.Config.Expiry),
 	}
 
-	connect.locks.Store(key, value)
+	this.locks.Store(key, value)
 
 	return nil
 }
-func (connect *defaultConnect) Unlock(key string) error {
-	connect.locks.Delete(key)
+func (this *defaultConnect) Unlock(key string) error {
+	this.locks.Delete(key)
 	return nil
 }
